3e720ccfe94523ee6bcd6dcc7f751000591eff5e
aprogergely <81948551+aprogergely@users.noreply.github.com>
2023-09-25 22:36:01 +0200

    v5
    leaderboard

--- Timer.py
+++ Timer.py
@@ -22,7 +22,7 @@ class GUI:
         self.toggle_button = tk.Button(root, command=self.toggle_timer, image=self.redbutton)#, background="red")
         self.toggle_button.grid(row=0, column=0, rowspan=3, sticky="nsew")
 
-        self.ranking_label = tk.Label(root, text="1. 0.000\n2. 0.234\n3. 0.999", font=("Arial", 12))
+        self.ranking_label = tk.Label(root, text=self.load_leaderboard(), font=("Arial", 12))
         self.ranking_label.grid(row=0, column=1, columnspan=1)
 
         self.timer_label = tk.Label(root, text="0.000 seconds", font=("Arial", 12))
@@ -64,6 +64,8 @@ class GUI:
             self.timer_label.config(text=formatted_time)
             self.toggle_button.config(image=self.graybutton)
             self.restart_button.grid(row=2, column=1, sticky="s")
+            self.update_leaderboard(formatted_time.strip(" seconds"))
+            self.ranking_label.config(text=self.load_leaderboard())
 
     def update_timer(self):
         if self.is_running:
@@ -74,12 +76,42 @@ class GUI:
         if not self.is_running:
             self.start_time = 0
             self.elapsed_time = 0
-            self.toggle_button.config(bg="red")
+            self.toggle_button.config(image=self.redbutton)
             #self.timer_label.config(text="0.000 seconds")
             self.is_over = False
-            self.toggle_button.grid_forget()
+            self.restart_button.grid_forget()
 
+    def load_leaderboard(self, filename="Leaderboard"):
+        try:
+            with open(filename + ".txt", 'r', encoding='utf-8-sig') as file:
+                file_content = file.readlines()
 
+            first, second, third = file_content[:3]
+        except:
+            first="99.999"
+            second="99.999"
+            third="99.999"
+
+        ranks = "1. "+first.strip()+"\n2. "+second.strip()+"\n3. "+third.strip()
+        return ranks
+    
+    def update_leaderboard(self, new_score, filename="Leaderboard"):
+        try:
+            with open(filename + ".txt", 'r', encoding='utf-8-sig') as file:
+                file_content = file.readlines()
+
+            first, second, third = file_content[:3]
+        except:
+            first=99.999
+            second=99.999
+            third=99.999
+
+        results=[float(first), float(second), float(third), float(new_score)]
+        results = sorted(results)
+        ranks = str(results[0]) + "\n" + str(results[1]) + "\n" + str(results[2]) + "\n"
+
+        with open(filename + ".txt", 'w', encoding='utf-8-sig') as file:
+            file.write(ranks)
 
 if __name__ == "__main__":
     root = tk.Tk()
